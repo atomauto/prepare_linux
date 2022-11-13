04826f189be4374ee9ae6bda6dae01ab149fb4eb
Konstantin <konstantin@atomauto.ru>
2022-11-13 20:25:25 +0600

    Print functions moved to file, motd.sh parallel getting info realised, giving 0.5-1 second speed up

--- motd.sh
+++ motd.sh
@@ -14,6 +14,7 @@
 #2.Some script logic expects english output of commands, if locale is other then english e.g. apt upgradeable grep isn't working
 LC_ALL=C
 LANG=C
+source print_functions.sh
 
 #Colors defined in .bashrc
 F1=$darkgray
@@ -21,165 +22,7 @@ F2=$lightpurple
 F3=$lightgreen
 F4=$red
 
-#Colors for messages
-CA="${CA:-\e[34m}" # Accent
-CO="${CO:-\e[32m}" # Ok
-CW="${CW:-\e[33m}" # Warning
-CE="${CE:-\e[31m}" # Error
-CN="${CN:-\e[0m}"  # None
-
-# Max width used for components in second column
-WIDTH="${WIDTH:-50}"
-
-# Prints given blocks of text side by side
-# $1 - left column
-# $2 - right column
-print_columns() {
-    [[ -z $2 ]] && return
-    paste <(echo -e "${CA}$1${1:+:}${CN}") <(echo -e "$2")
-}
-
-# Prints text with color according to given value and two thresholds
-# $1 - text to print
-# $2 - current value
-# $3 - warning threshold
-# $4 - error threshold
-print_color() {
-    local out=""
-    if (($(bc -l <<<"$2 < $3"))); then
-        out+="${CO}"
-    elif (($(bc -l <<<"$2 >= $3 && $2 < $4"))); then
-        out+="${CW}"
-    else
-        out+="${CE}"
-    fi
-    out+="$1${CN}"
-    echo "${out}"
-}
-
-# Prints text as either acitve or inactive
-# $1 - text to print
-# $2 - literal "active" or "inactive"
-print_status() {
-    local out=""
-    if [[ $2 == "active" ]]; then
-        out+="${CO}▲${CN}"
-    else
-        out+="${CE}▼${CN}"
-    fi
-    out+=" $1${CN}"
-    echo "${out}"
-}
-
-# Prints one line of text, truncates it at specified width and add ellipsis.
-# Truncation can occur either at the start or at the end of the string.
-# $1 - line to print
-# $2 - width limit
-# $3 - "start" or "end", default "end"
-print_truncate() {
-    local out
-    local new_length=$(($2 - 1))
-    # Just echo the string if it's shorter than the limit
-    if [[ ${#1} -le "$2" ]]; then
-        out="$1"
-    elif [[ -z "$3" || "$3" == "end" ]]; then
-        out="${1::${new_length}}…"
-    else
-        out="…${1: -${new_length}}"
-    fi
-    echo "${out}"
-}
-
-# Prints given text n times
-# $1 - text to print
-# $2 - how many times to print
-print_n() {
-    local out=""
-    for ((i = 0; i < $2; i++)); do
-        out+="$1"
-    done
-    echo "${out}"
-}
-
-# Prints bar divided in two parts by given percentage
-# $1 - bar width
-# $2 - percentage
-print_bar() {
-    local bar_width=$(($1 - 2))
-    local used_width=$(($2 * bar_width / 100))
-    local free_width=$((bar_width - used_width))
-    local out=""
-    out+="["
-    out+="${CE}"
-    out+=$(print_n "=" ${used_width})
-    out+="${CO}"
-    out+=$(print_n "=" ${free_width})
-    out+="${CN}"
-    out+="]"
-    echo "${out}"
-}
-
-
-# Prints comma-separated arguments wrapped to the given width
-# $1 - width to wrap to
-# $2, $3, ... - values to print
-print_wrap() {
-    local width=$1
-    shift
-    local out=""
-    local line_length=0
-    for element in "$@"; do
-        element="${element},"
-        local visible_elelement future_length
-        visible_elelement=$(strip_ansi "${element}")
-        future_length=$((line_length + ${#visible_elelement}))
-        if [[ ${line_length} -ne 0 && ${future_length} -gt ${width} ]]; then
-            out+="\n"
-            line_length=0
-        fi
-        out+="${element} "
-        line_length=$((line_length + ${#visible_elelement}))
-    done
-    [[ -n "${out}" ]] && echo "${out::-2}"
-}
-
-# Prints some text justified to left and some justified to right
-# $1 - total width
-# $2 - left text
-# $3 - right text
-print_split() {
-    local visible_first visible_second invisible_first_width invisible_second_width total_width \
-        first_half_width second_half_width format_string
-
-    visible_first=$(strip_ansi "$2")
-    visible_second=$(strip_ansi "$3")
-    invisible_first_width=$((${#2} - ${#visible_first}))
-    invisible_second_width=$((${#3} - ${#visible_second}))
-    total_width=$(($1 + invisible_first_width + invisible_second_width))
-
-    if ((${#visible_first} + ${#visible_second} < $1)); then
-        first_half_width=${#2}
-    else
-        first_half_width=$(($1 / 2))
-    fi
-    second_half_width=$((total_width - first_half_width))
-
-    format_string="%-${first_half_width}s%${second_half_width}s"
-    # shellcheck disable=SC2059
-    printf ${format_string} "${2:0:${first_half_width}}" "${3:0:${second_half_width}}"
-}
-
-# Strips ANSI color codes from given string
-# $1 - text to strip
-strip_ansi() {
-    echo -e "$1" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
-}
-
-function show_system_info() {
-    #Get my fqdn hostname.domain.name.tld
-    #HOSTNAME=$(hostname --fqdn)
-    #username@hostname looks better
-    HOSTNAME=$(id -un)@$(hostname)
+function get_local_ip() {
     #Get local ip
     # Local IP interface, by default we suggest 'auto' (interface on default route)
     # Possible values:  'auto', 'en0', 'en1'
@@ -203,6 +46,9 @@ function show_system_info() {
             fi
         done
     fi
+}
+
+function get_public_ip() {
     #Get public ip
     #Security warning: we connect to external site in internet
     #TODO: getting public IP takes 1-1.5 seconds
@@ -210,26 +56,19 @@ function show_system_info() {
         public_ip="$(dig +time=1 +tries=1 +short myip.opendns.com @resolver1.opendns.com)"
         [[ "$public_ip" =~ ^\; ]] && unset public_ip
     fi
-
     if [[ ! -n "$public_ip_host" ]] && [[ -z "$public_ip" ]] && type -p drill >/dev/null; then
         public_ip="$(drill myip.opendns.com @resolver1.opendns.com |
             awk '/^myip\./ && $3 == "IN" {print $5}')"
     fi
-
     if [[ -z "$public_ip" ]] && type -p curl >/dev/null; then
         public_ip="$(curl -L --max-time "$public_ip_timeout" -w '\n' "$public_ip_host")"
     fi
-
     if [[ -z "$public_ip" ]] && type -p wget >/dev/null; then
         public_ip="$(wget -T "$public_ip_timeout" -qO- "$public_ip_host")"
     fi
-    #Get current kernel version
-    UNAME=$(uname -r)
-    #Get runnig sles distribution name
-    DISTRIBUTION=$(lsb_release -s -d)
-    #Get hardware platform
-    PLATFORM=$(uname -m)
+}
 
+function get_uptime() {
     #UPTIME=$(uptime | cut -c2- | cut -d, -f1)
     #More pretty uptime from neofetch
     if [[ -r /proc/uptime ]]; then
@@ -254,12 +93,9 @@ function show_system_info() {
     uptime=${d:+$d, }${h:+$h, }$m
     uptime=${uptime%', '}
     uptime=${uptime:-$s seconds}
+}
 
-    ## get amount of cpu processors
-    CPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
-    ## get system cpu model
-    CPUMODEL=$(cat /proc/cpuinfo | egrep 'model name' | uniq | awk -F ': ' {'print $2'})
-
+function get_memory() {
     # Get current free memory
     # MEMFREE=$(echo $(cat /proc/meminfo | egrep MemFree | awk {'print $2'})/1024 | bc)
     # Get maxium usable memory
@@ -294,27 +130,9 @@ function show_system_info() {
     # mem_total=$((mem_total * 1024))
     # mem_label=KiB
     memory="${mem_used}${mem_label:-MiB} / ${mem_total}${mem_label:-MiB} ${mem_perc:+(${mem_perc}%)}"
+}
 
-    ## get current free swap space
-    SWAPFREE=$(echo $(cat /proc/meminfo | egrep SwapFree | awk {'print $2'})/1024 | bc)
-    ## get maxium usable swap space
-    SWAPMAX=$(echo $(cat /proc/meminfo | egrep SwapTotal | awk {'print $2'})/1024 | bc)
-    #Loads average
-    loads=$(cut -d ' ' -f '1,2,3' /proc/loadavg)
-    nproc=$(nproc)
-    warning_threshold=$(bc -l <<<"${nproc} * 0.9")
-    error_threshold=$(bc -l <<<"${nproc} * 1.5")
-    load_average=""
-    for load in ${loads}; do
-        load_average+="$(print_color "${load}" "${load}" "${warning_threshold}" "${error_threshold}"), "
-    done
-    # print_columns "Load average" "${text::-2}"
-
-    #Get current procs
-    PROCCOUNT=$(ps -Afl | egrep -v 'ps|wc' | wc -l)
-    #Get maxium usable procs
-    PROCMAX=$(ulimit -u)
-
+function get_model() {
     if [[ -d /system/app/ && -d /system/priv-app ]]; then
         model="$(getprop ro.product.brand) $(getprop ro.product.model)"
 
@@ -353,12 +171,64 @@ function show_system_info() {
     "Standard PC"*) model="KVM/QEMU (${model})" ;;
     OpenBSD*) model="vmm ($model)" ;;
     esac
+}
+
+function get_loads_average() {
+    #Loads average
+    loads=$(cut -d ' ' -f '1,2,3' /proc/loadavg)
+    nproc=$(nproc)
+    warning_threshold=$(bc -l <<<"${nproc} * 0.9")
+    error_threshold=$(bc -l <<<"${nproc} * 1.5")
+    load_average=""
+    for load in ${loads}; do
+        load_average+="$(print_color "${load}" "${load}" "${warning_threshold}" "${error_threshold}"), "
+    done
+    # print_columns "Load average" "${text::-2}"
+}
+
+function generate_system_info() {
+    #Get my fqdn hostname.domain.name.tld
+    #HOSTNAME=$(hostname --fqdn)
+    #username@hostname looks better
+    HOSTNAME=$(id -un)@$(hostname)
+    #Get current kernel version
+    UNAME=$(uname -r)
+    #Get runnig sles distribution name
+    DISTRIBUTION=$(lsb_release -s -d)
+    #Get hardware platform
+    PLATFORM=$(uname -m)
+    #Get amount of cpu processors
+    CPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
+    #Get system cpu model
+    CPUMODEL=$(cat /proc/cpuinfo | egrep 'model name' | uniq | awk -F ': ' {'print $2'})
+    #Get current free swap space
+    SWAPFREE=$(echo $(cat /proc/meminfo | egrep SwapFree | awk {'print $2'})/1024 | bc)
+    #Get maxium usable swap space
+    SWAPMAX=$(echo $(cat /proc/meminfo | egrep SwapTotal | awk {'print $2'})/1024 | bc)
+    #Get current procs
+    PROCCOUNT=$(ps -Afl | egrep -v 'ps|wc' | wc -l)
+    #Get maxium usable procs
+    PROCMAX=$(ulimit -u)
+
+    # #Async functions calls
+    # get_local_ip &
+    # get_public_ip &
+    # get_uptime &
+    # get_memory &
+    # get_model &
+    # get_loads_average &
+    # wait
+    get_local_ip 
+    get_public_ip 
+    get_uptime 
+    get_memory 
+    get_model 
+    get_loads_average 
 
     #Display system information
     #TODO: change relevant string colour if load average/cpu/memory usage is high
     echo -e "
 ${F2}============[ ${F1}System Info${F2} ]====================================================
-${F1}        Hostname ${F2}= ${F3}$HOSTNAME
 ${F1}            Host ${F2}= ${F3}$model
 ${F1}              OS ${F2}= ${F3}$DISTRIBUTION ${PLATFORM}
 ${F1}          Kernel ${F2}= ${F3}$UNAME
@@ -369,7 +239,7 @@ ${F1}             CPU ${F2}= ${F3}$CPUS x $CPUMODEL
 ${F1}    Load average ${F2}= ${F3}$load_average
 ${F1}          Memory ${F2}= ${F3}$memory
 ${F1}     Swap Memory ${F2}= ${F3}$SWAPFREE MB Free of $SWAPMAX MB Total
-${F1}       Processes ${F2}= ${F3}$PROCCOUNT of $PROCMAX MAX${F1}"
+${F1}       Processes ${F2}= ${F3}$PROCCOUNT of $PROCMAX MAX${F1}">system_info
 }
 
 ## Storage Informations
@@ -382,7 +252,7 @@ ${F2}============[ ${F1}Storage Info${F2} ]=====================================
 ${F3}${STORAGE}${F1}"
 }
 
-function show_disk_info() {
+function generate_disk_info() {
     excluded_types=(
         "devtmpfs"
         "ecryptfs"
@@ -391,7 +261,7 @@ function show_disk_info() {
     )
     # shellcheck disable=SC2046
     disks="$(df -h --local --print-type $(printf " -x %s" "${excluded_types[@]}") | tail -n +2 | sort -u -k 7)"
-    text=""
+    disk_info=""
     while IFS= read -r disk; do
         IFS=" " read -r filesystem _ total used free percentage mountpoint <<<"${disk}"
 
@@ -403,9 +273,10 @@ function show_disk_info() {
         left_label="${device} (${mountpoint}) - ${used} used, ${free} free"
 
         label=$(print_split "${WIDTH}" "${left_label}" "${right_label}")
-        text+="${label}\n$(print_bar "${WIDTH}" "${percentage::-1}")\n"
+        disk_info+="${label}\n$(print_bar "${WIDTH}" "${percentage::-1}")\n"
     done <<<"${disks}"
-    print_columns "Disk space" "${text::-2}"
+        print_columns "Disk space" "${disk_info::-2}">disk_info
+
 }
 
 #User Informations
@@ -453,7 +324,7 @@ ${F1}      Environment ${F2}= ${F3}$SYSENV"
 }
 
 #Services status and info
-function show_services_info() {
+function generate_services_info() {
     declare -A services
     services["nginx"]="Nginx"
     services["php-fpm"]="PHP"
@@ -477,19 +348,19 @@ function show_services_info() {
         fi
     done
 
-    text=$(print_wrap "${WIDTH}" "${statuses[@]}")
+    services_info=$(print_wrap "${WIDTH}" "${statuses[@]}")
+        print_columns "Services" "${services_info}">services
 
-    print_columns "Services" "${text}"
 }
 
-function show_docker_info() {
+function generate_docker_info() {
     #If there is no docker, do nothing
     #Suspend type command output by redirecting std pipe and error pipe to /dev/null
     if type docker >/dev/null 2>&1; then
         containers=$(docker ps -a --format "{{ .Names }}\t{{ .Status }}\t{{ .State }}")
-        text=""
+        docker_info=""
         if [[ -z "${containers}" ]]; then
-            text+="no containers\n"
+            docker_info+="no containers\n"
         else
             while IFS= read -r line; do
                 IFS=$'\t' read -r name description state <<<"${line}"
@@ -499,16 +370,15 @@ function show_docker_info() {
                 exited | dead) color="${CE}" ;;
                 *) color="${CN}" ;;
                 esac
-                text+="$(print_split "${WIDTH}" "${name}" "${color}${description,,}${CN}")\n"
+                docker_info+="$(print_split "${WIDTH}" "${name}" "${color}${description,,}${CN}")\n"
             done <<<"${containers}"
         fi
-
-        print_columns "Docker" "${text::-2}"
+        print_columns "Docker" "${docker_info::-2}">docker
     fi
 
 }
 
-function show_updates_info() {
+function generate_updates_info() {
     if type checkupdates >/dev/null 2>&1; then
         updates=$(checkupdates 2>/dev/null | wc -l)
         if type yay >/dev/null 2>&1; then
@@ -522,25 +392,26 @@ function show_updates_info() {
     else
         updates="N/A"
     fi
-    text="$(print_color "${updates} available" ${updates} 1 50)"
-    print_columns "Updates" "${text}"
+    updates_info="$(print_color "${updates} available" ${updates} 1 50)"
+        print_columns "Updates" "${updates_info}">updates
+
 }
 function show_info() {
-    show_system_info
-    show_disk_info
-    show_services_info
-    show_docker_info
-    show_updates_info
-
     #TODO: implement parallel execution, but save output order
     #For this we need save temporary output and divide all in smaller functions
-    # show_system_info &
-    # show_storage_info &
-    # show_services_info &
-    # show_docker_info &
-    # show_updates_info &
-    # wait
+    generate_system_info &
+    generate_disk_info &
+    generate_services_info &
+    generate_docker_info &
+    generate_updates_info &
+    wait
 
+    cat system_info
+    cat disk_info
+    cat services
+    cat docker
+    cat updates
+    
     # show_user_info
     # show_update_info
     # show_environment_info
