71de31c0d0569d8721bfb913efdd11a1b422db3c
Konstantin <konstantin@atomauto.ru>
2023-01-12 18:08:23 +0400

    nginx.conf bugfix

--- nginx.sh
+++ nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt install -y nginx-light php-fpm php-pear php-cgi php-common php-mbstring php-zip php-net-socket php-gd php-xml-util php-mysql php-bcmath unzip
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 service nginx restart
 chown -R www-data:www-data /var/www
 apt install -y logrotate
-echo "server_tokens off;" >> /etc/nginx/nginx.conf
 echo "php_admin_flag[expose_php] = off" >> /etc/php/7.4/fpm/php-fpm.conf
 service php7.4-fpm restart && service nginx restart
