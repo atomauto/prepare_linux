81fac49ab3befde705ddd7c4fd4cf4971fc0b87e
Konstantin <konstantin@atomauto.ru>
2024-04-14 18:14:38 +0400

    fix nomad install

--- nomad.sh
+++ nomad.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt install gpg -y
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt update && apt install -y nomad vault consul >> $logfile
